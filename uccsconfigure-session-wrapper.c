c9967bd1db22e665744e9752acacd659911c3276
Ted Gould <ted@gould.cx>
2012-09-14 14:28:15 -0500

    Fleshing out the wrapper

--- uccsconfigure-session-wrapper.c
+++ uccsconfigure-session-wrapper.c
@@ -1,7 +1,14 @@
+#include <stdlib.h>
+#include <unistd.h>
 
 int
 main (int argc, char * argv[])
 {
+	char * args[2];
+	args[0] = PKGDATADIR "/uccsconfigure-session";
+	args[1] = NULL;
+
+	execvp(args[0], args);
 
 	return 0;
 }
